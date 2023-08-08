f3fa9ba819be396c8f4ed6359a00ce12d0418d8e
vdud <varundudeja96@gmail.com>
2023-08-08 18:02:24 +0530

    email-bug

--- src/routes/api/sendMessage/+server.ts
+++ src/routes/api/sendMessage/+server.ts
@@ -6,16 +6,18 @@ import { contact } from '$lib/db/collection';
 export const POST: RequestHandler = async ({ request }) => {
 	const { name, email, phoneNumber, $contactMessage } = await request.json();
 
+	const formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+
 	await sendEmail(
-		'varundudeja96@gmail.com',
+		'varundudeja95@gmail.com',
 		'You have a new message from your website!',
-		`Name: ${name} \nEmail: ${email} \nPhone Number: +91 ${phoneNumber} \nMessage: ${$contactMessage}`
+		`Name: ${formattedName} \nEmail: ${email} \nPhone Number: +91 ${phoneNumber} \nMessage: ${$contactMessage}`
 	);
 
 	await sendEmail(
 		email,
 		'Regarding your message to Varun Dudeja (varundudeja.com)',
-		`Hey ${name} \n\nThankyou for your Interest in my services, \nI will get back to you as soon as possible. \n\nplease dont reply to this message, this is an automated message. \n\nGreetings, \nVarun Dudeja `
+		`Hey ${formattedName} \n\nThankyou for your Interest in my services, \nI will get back to you as soon as possible. \n\nplease dont reply to this message, this is an automated message. \n\nGreetings, \nVarun Dudeja `
 	);
 
 	await contact.insertOne({
